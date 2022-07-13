0fb916f881948ef8cee826587c08b7823bcf8e57
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-07-13 17:53:23 +0300

    Added category component.
    Added category component.

--- components/Categories.js
+++ components/Categories.js
@@ -1,7 +1,36 @@
-import React from 'react'
+
+import { useState, useEffect,React } from 'react';
+import categoryData from 'components/api/categories.json'
+import Category from './ui/Category';
+
+
 
 export default function Categories() {
+
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+      // istek
+      // setTimeout(() => setCategories(categoryData), 1000)
+      setCategories(categoryData)
+  }, [])
+
+
   return (
-	<div>Categories</div>
+	<div className='bg-gray-100 py-4'>
+    <div className='container mx-auto'>
+        <h3 className='text-sm font-semibold mb-3'>Kategoriler</h3>
+        <div className='flex flex-row  gap-x-8 gap-y-8 flex-wrap justify-start'>
+        {categories.map((item,key)=>{
+          return(
+            <div> 
+              <Category key={key} category={item} />
+              </div>
+          );
+        })}
+        </div>
+    </div>
+  </div>
   )
 }
