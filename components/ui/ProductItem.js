6de7a2cfb5418cda10e6a8b3ecfd50c3753e8c2f
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-07-14 17:49:48 +0300

    ufak bir hata düzenlendi

--- components/ui/ProductItem.js
+++ components/ui/ProductItem.js
@@ -2,8 +2,8 @@ import { FiPlus } from 'react-icons/fi';
 
 export default function ProductItem({ product }) {
     return (
-        <div className="bg-white flex flex-col gap-y-1 relative items-center text-center text-sm font-semibold p-3 w-[150px]">
-            <button className="absolute top-2 right-6 bg-white w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg transition-colors text-brand-color shadow-md hover:border-brand-color">
+        <div className="bg-white flex flex-col gap-y-1 relative items-center text-center text-sm font-semibold p-3">
+            <button className="absolute top-3 right-3 bg-white w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg transition-colors text-brand-color shadow-md hover:border-brand-color">
                 <FiPlus size={16} />
             </button>
             <img src={product.image} alt={product.title} className="" />
