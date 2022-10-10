dc66dd575782a2d74ed89a9d62e698b78fd53bfa
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-10-10 13:18:40 +0300

    getir bi mutluluk git add .

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
       <div className="container mx-auto h-11 flex items-center justify-between align-middle">
       <div className="flex flex-row justify-between gap-x-2 md:gap-x-3">
       <a href="#">
-          <img alt="getir-logo" className="pt-[2px]" src="/getir.png" width={46} />
+          <img alt={`${process.env.NEXT_PUBLIC_FAVICON}/getir-logo`} className="pt-[2px]" src="/getir.png" width={46} />
         </a>
         <a href="#" className="text-white hover:text-yellow-400 transition-all"> <span className="text-xs md:text-sm">  GetirYemek </span>     </a>
         <a href="#" className="text-white hover:text-yellow-400 transition-all"> <span className="text-xs md:text-sm">  GetirBüyük </span>     </a>
