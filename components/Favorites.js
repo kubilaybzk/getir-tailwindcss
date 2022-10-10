a4564cf59b7b6a944cf50a4f50b279edf488db60
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-10-10 13:36:13 +0300

    getir bi mutluluk

--- components/Favorites.js
+++ components/Favorites.js
@@ -14,7 +14,7 @@ export default function Favorites() {
     return (
         <div className="container mx-auto ">
             <Title>Favoriler</Title>
-            <div className="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-11 2xl:grid-cols-12">
+            <div className="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-6 lg:grid-cols-10 xl:grid-cols-11 2xl:grid-cols-12">
                 {products.length && products.map(product => <ProductItem key={product.id} product={product} />)}
             </div>
         </div>
